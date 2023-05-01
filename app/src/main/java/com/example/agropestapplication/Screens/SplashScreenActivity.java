4770a86d9a1cd7a5e47c02d9fef8de1d555754d5
agro-pest <sathsara.psf99@gmail.com>
2023-05-01 14:18:05 +0530

    Add event listener to go to login screen

--- app/src/main/java/com/example/agropestapplication/Screens/SplashScreenActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/SplashScreenActivity.java
@@ -1,16 +1,34 @@
 package com.example.agropestapplication.Screens;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.agropestapplication.MainActivity;
 import com.example.agropestapplication.R;
+import com.ncorti.slidetoact.SlideToActView;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+
+    SlideToActView slideToActView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        slideToActView = findViewById(R.id.swipeToUnlock);
+
+       slideToActView.setOnSlideCompleteListener(new SlideToActView.OnSlideCompleteListener() {
+           @Override
+           public void onSlideComplete(@NonNull SlideToActView slideToActView) {
+              startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+           }
+       });
+
     }
 }
