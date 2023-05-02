3aebd44805093d2a815cbd047ba9d33769c51d4c
“[firstname <srimalisewwandika1998@gmail.com>
2023-05-02 18:18:33 +0530

    add comment

--- app/src/main/java/com/example/agropestapplication/Screens/DashboardActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/DashboardActivity.java
@@ -30,6 +30,7 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
 
+
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
         drawerButton = findViewById(R.id.drawerButton);
