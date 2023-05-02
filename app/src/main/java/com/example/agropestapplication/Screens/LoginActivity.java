557defd040d22b348ebca3385d464cc0dfd8d9b5
agro-pest <sathsara.psf99@gmail.com>
2023-05-02 12:07:00 +0530

    Implement signup button

--- app/src/main/java/com/example/agropestapplication/Screens/LoginActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/LoginActivity.java
@@ -1,16 +1,55 @@
 package com.example.agropestapplication.Screens;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.agropestapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button signUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signUp = findViewById(R.id.btnSignUp);
+
+        // Implement sign up button
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+            }
+        });
+
+    }
+
+    // Implement device back button.
+    // if the user click  the back button, user can see the alert dialog box
+    public void onBackPressed(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(LoginActivity.this);
+        alertDialog.setTitle("Exit App");
+        alertDialog.setMessage("Do you want to exit this app ?");
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+            }
+        });
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 }
