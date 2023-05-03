b3b8ca408b289260b90597832f8b53f9ef119605
agro-pest <sathsara.psf99@gmail.com>
2023-05-03 08:31:36 +0530

    Implement navigation button to pesticides screen

--- app/src/main/java/com/example/agropestapplication/Screens/DashboardActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/DashboardActivity.java
@@ -3,10 +3,12 @@ package com.example.agropestapplication.Screens;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,6 +24,7 @@ public class DashboardActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     ImageButton drawerButton;
+    CardView pesticides;
 
     @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
@@ -34,6 +37,7 @@ public class DashboardActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
         drawerButton = findViewById(R.id.drawerButton);
+        pesticides = findViewById(R.id.pesticides);
         //Implement drawer button to get drawer menu
 
         drawerButton.setOnClickListener(new View.OnClickListener() {
@@ -48,12 +52,19 @@ public class DashboardActivity extends AppCompatActivity {
                 this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 return false;
             }
         });
+
+        pesticides.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),PesticidesActivity.class));
+            }
+        });
+
 }
 }
