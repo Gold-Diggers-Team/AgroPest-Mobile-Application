e288270f184364e8e4e84de1aaf533e3f13e0199
agro-pest <sathsara.psf99@gmail.com>
2023-05-03 08:57:44 +0530

    Clean the unnecessary spaces inside the code

--- app/src/main/java/com/example/agropestapplication/Screens/DashboardActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/DashboardActivity.java
@@ -3,10 +3,12 @@ package com.example.agropestapplication.Screens;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,7 +24,8 @@ public class DashboardActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     ImageButton drawerButton;
-
+    CardView pesticides,fertilizer,profile;
+   
     @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +33,14 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
 
-
+        fertilizer = findViewById(R.id.fertilizer);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
         drawerButton = findViewById(R.id.drawerButton);
+        pesticides = findViewById(R.id.pesticides);
+        profile = findViewById(R.id.profile);
+        
         //Implement drawer button to get drawer menu
-
         drawerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,12 +53,34 @@ public class DashboardActivity extends AppCompatActivity {
                 this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 return false;
             }
         });
+
+        //implement pesticides screen open button
+        pesticides.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),PesticidesActivity.class));
+            }
+        });
+
+        //implement fertilizer screen open button
+        fertilizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),FertilizerActivity.class));
+                
+        //Implement profile button
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+            }
+        });
+
 }
 }
