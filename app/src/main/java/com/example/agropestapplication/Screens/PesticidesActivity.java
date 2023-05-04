86198a4c830d6f5fff25bd51b14704b95bed8e56
agro-pest <sathsara.psf99@gmail.com>
2023-05-04 23:22:21 +0530

    commit

--- app/src/main/java/com/example/agropestapplication/Screens/PesticidesActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/PesticidesActivity.java
@@ -2,15 +2,35 @@ package com.example.agropestapplication.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.agropestapplication.R;
 
 public class PesticidesActivity extends AppCompatActivity {
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesticides);
+
+        GradientDrawable gradientDrawable = new GradientDrawable(
+                GradientDrawable.Orientation.TL_BR,
+                new int[] {Color.parseColor("#1FA0FF"), Color.parseColor("#12DAFB"), Color.parseColor("#A7FDCC")}
+        );
+        View myView = findViewById(R.id.my_view);
+        View myView2 = findViewById(R.id.my_view2);
+        View myView3 = findViewById(R.id.my_view3);
+        View myView4 = findViewById(R.id.my_view4);
+
+
+        myView.setBackground(gradientDrawable);
+        myView2.setBackground(gradientDrawable);
+        myView3.setBackground(gradientDrawable);
+        myView4.setBackground(gradientDrawable);
+
     }
 }
