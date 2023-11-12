0310fc72ac0191ac41f3cfb4ff52918e8c927088
agro-pest <sathsara.psf99@gmail.com>
2023-11-12 18:52:27 +0530

    commit

--- app/src/main/java/com/example/agropestapplication/Screens/LoginActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/LoginActivity.java
@@ -1,22 +1,16 @@
 package com.example.agropestapplication.Screens;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.agropestapplication.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
