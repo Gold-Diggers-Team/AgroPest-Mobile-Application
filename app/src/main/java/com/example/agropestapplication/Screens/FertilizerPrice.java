ee2e1815a7eb40b0d1cab53ed0b86cf563d3a6e5
agro-pest <sathsara.psf99@gmail.com>
2023-11-12 18:58:09 +0530

    Create fertilizer screen

---
+++ app/src/main/java/com/example/agropestapplication/Screens/FertilizerPrice.java
@@ -0,0 +1,16 @@
+package com.example.agropestapplication.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agropestapplication.R;
+
+public class FertilizerPrice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fertilizer_price);
+    }
+}
