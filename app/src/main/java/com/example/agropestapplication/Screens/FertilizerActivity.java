f434561c35c1bd553d5c84d6331572aa91cf7ada
agro-pest <sathsara.psf99@gmail.com>
2023-11-12 19:19:44 +0530

    add click listener to cardView to navigate the FertilizerPrice screen

--- app/src/main/java/com/example/agropestapplication/Screens/FertilizerActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/FertilizerActivity.java
@@ -1,16 +1,29 @@
 package com.example.agropestapplication.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.agropestapplication.R;
 
 public class FertilizerActivity extends AppCompatActivity {
 
     @Override
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fertilizer);
+        CardView checkFertilizer = findViewById(R.id.checkFertilizer);
+
+        checkFertilizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),FertilizerPrice.class));
+            }
+        });
     }
 }
