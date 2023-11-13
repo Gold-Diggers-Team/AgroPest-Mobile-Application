9ae3fe3b7f9629bd58958cf0808ce0cb8cb62d59
agro-pest <sathsara.psf99@gmail.com>
2023-11-13 22:22:24 +0530

    Implement agriInfo button

--- app/src/main/java/com/example/agropestapplication/Screens/AgriServiceActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/AgriServiceActivity.java
@@ -1,16 +1,61 @@
 package com.example.agropestapplication.Screens;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.agropestapplication.Adapter.Adapter;
+import com.example.agropestapplication.Adapter.AgricultureServiceAdapter;
+import com.example.agropestapplication.Model.ModelClass;
 import com.example.agropestapplication.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class AgriServiceActivity extends AppCompatActivity {
 
+
+    RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+    AgricultureServiceAdapter adapter;
+    ArrayList<ModelClass> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agri_service);
+        recyclerView = findViewById(R.id.items);
+        databaseReference = FirebaseDatabase.getInstance().getReference("form");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        adapter = new AgricultureServiceAdapter(this, list);
+        recyclerView.setAdapter(adapter);
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    ModelClass modelClass = dataSnapshot.getValue(ModelClass.class);
+                    list.add(modelClass);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
-}
+    }
