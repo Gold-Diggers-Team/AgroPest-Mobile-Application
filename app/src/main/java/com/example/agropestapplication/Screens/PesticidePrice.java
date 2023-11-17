e802c0d6beba649687f4405909b31bfe9ae91d80
agro-pest <sathsara.psf99@gmail.com>
2023-11-17 16:59:49 +0530

    Implement push notification

--- app/src/main/java/com/example/agropestapplication/Screens/PesticidePrice.java
+++ app/src/main/java/com/example/agropestapplication/Screens/PesticidePrice.java
@@ -2,23 +2,36 @@ package com.example.agropestapplication.Screens;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.agropestapplication.Adapter.Adapter;
 import com.example.agropestapplication.Model.ModelClass;
 import com.example.agropestapplication.R;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.ArrayList;
 
 public class PesticidePrice extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "channel_id";
+    private static final int NOTIFICATION_ID = 1;
 
     RecyclerView recyclerView;
     DatabaseReference databaseReference;
@@ -39,26 +52,82 @@ public class PesticidePrice extends AppCompatActivity {
         adapter = new Adapter(this,list);
         recyclerView.setAdapter(adapter);
 
+        // Set up Firebase Cloud Messaging
+        FirebaseApp.initializeApp(this);
+
+        // Add this line to subscribe to a topic (you can use a topic related to your new items)
+        FirebaseMessaging.getInstance().subscribeToTopic("new_items");
+
+        createNotificationChannel();
+
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                ArrayList<ModelClass> newList = new ArrayList<>();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     ModelClass modelClass = dataSnapshot.getValue(ModelClass.class);
-                    list.add(modelClass);
+                    newList.add(modelClass);
                 }
-                adapter.notifyDataSetChanged();
+
+                for (ModelClass newItem : newList) {
+                    boolean isNewItem = true;
+                    for (ModelClass existingItem : list) {
+                        if (newItem.getName().equals(existingItem.getName()) && newItem.getPrice() == existingItem.getPrice()) {
+                            isNewItem = false;
+                            break;
+                        }
+                    }
+
+                    if (isNewItem) {
+                        sendFCMNotification("New Item Added", "A new pesticides has been added Please check.");
+
+                        list.add(newItem);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                // Handle onCancelled event
             }
         });
 
+    }
+
+    private void sendFCMNotification(String title, String body) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
 
 
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Channel Name";
+            String description = "Channel Description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
 
-
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
