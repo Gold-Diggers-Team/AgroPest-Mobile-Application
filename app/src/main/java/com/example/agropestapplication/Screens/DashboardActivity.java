e0087342e8ebeace2d557203b0e4b96a2a9d08bd
agro-pest <sathsara.psf99@gmail.com>
2023-11-17 17:34:15 +0530

    Implement device back button

--- app/src/main/java/com/example/agropestapplication/Screens/DashboardActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/DashboardActivity.java
@@ -1,6 +1,7 @@
 package com.example.agropestapplication.Screens;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
@@ -203,5 +205,25 @@ public class DashboardActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), AgriServiceActivity.class));
             }
         });
+    }
+    // Implement device back button.
+    // if the user click  the back button, user can see the alert dialog box
+    public void onBackPressed(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(DashboardActivity.this);
+        alertDialog.setTitle("Exit App");
+        alertDialog.setMessage("Do you want to exit this app ?");
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+            }
+        });
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 }
