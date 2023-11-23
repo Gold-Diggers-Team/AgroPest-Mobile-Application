708ecee117880af60416692df2b88bdd20e4e1d5
agro-pest <sathsara.psf99@gmail.com>
2023-11-23 12:55:51 +0530

    Implement navigation to the about fertilizer

--- app/src/main/java/com/example/agropestapplication/Screens/FertilizerActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/FertilizerActivity.java
@@ -19,6 +19,7 @@ public class FertilizerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fertilizer);
         CardView checkFertilizer = findViewById(R.id.checkFertilizer);
         CardView detailsFertilizer = findViewById(R.id.detailsFertilizer);
+        CardView about = findViewById(R.id.about);
 
         checkFertilizer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +31,12 @@ public class FertilizerActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), DetailsFertilizerCropActivity.class));
+            }
+        });
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AboutFertlizerActivity.class));
             }
         });
     }
