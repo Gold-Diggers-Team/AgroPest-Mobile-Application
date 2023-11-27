f7e5c6d92a4ef1bafe86197bbd92434b3195a586
agro-pest <sathsara.psf99@gmail.com>
2023-11-27 20:21:43 +0530

    Implement notification

--- app/src/main/java/com/example/agropestapplication/Screens/PesticidePrice.java
+++ app/src/main/java/com/example/agropestapplication/Screens/PesticidePrice.java
@@ -94,6 +94,7 @@ public class PesticidePrice extends AppCompatActivity {
 
                     if (isNewItem) {
                         list.add(newItem);
+                        sendFCMNotification("New Item Added", "A new Pesticide has been added Please check.");
                         adapter.notifyDataSetChanged();
                     }
                 }
@@ -105,6 +106,27 @@ public class PesticidePrice extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void sendFCMNotification(String title, String body) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 
     private void createNotificationChannel() {
