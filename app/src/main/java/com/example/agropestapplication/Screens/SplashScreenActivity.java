71055cc4b6e1a2990111db4c81312e1b4349051e
agro-pest <sathsara.psf99@gmail.com>
2023-11-27 20:22:36 +0530

    Implement Check internet connection when start the app

--- app/src/main/java/com/example/agropestapplication/Screens/SplashScreenActivity.java
+++ app/src/main/java/com/example/agropestapplication/Screens/SplashScreenActivity.java
@@ -1,13 +1,19 @@
 package com.example.agropestapplication.Screens;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
 
 import com.example.agropestapplication.R;
 import com.ncorti.slidetoact.SlideToActView;
@@ -15,45 +21,95 @@ import com.ncorti.slidetoact.SlideToActView;
 @SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
-
     SlideToActView slideToActView;
+    AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        //Bind elements to activity from xml
+        // Check internet connection
+        if (isNetworkAvailable()) {
+            // Internet connection is available, proceed with your app logic
+            setupSlideToUnlock();
+        } else {
+            // No internet connection, show a warning message
+            showNoInternetDialog();
+        }
+
+        // Register BroadcastReceiver for network state changes
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkStateReceiver, intentFilter);
+    }
+
+    private void setupSlideToUnlock() {
+        // Bind elements to activity from xml
         slideToActView = findViewById(R.id.swipeToUnlock);
         slideToActView.setBumpVibration(50);
 
-        //implement slide to unlock button
-       slideToActView.setOnSlideCompleteListener(new SlideToActView.OnSlideCompleteListener() {
-           @Override
-           public void onSlideComplete(@NonNull SlideToActView slideToActView) {
-              startActivity(new Intent(getApplicationContext(),LoginActivity.class));
-           }
-       });
-
+        // Implement slide to unlock button
+        slideToActView.setOnSlideCompleteListener(new SlideToActView.OnSlideCompleteListener() {
+            @Override
+            public void onSlideComplete(@NonNull SlideToActView slideToActView) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
     }
-    // Implement device back button.
-    // if the user click  the back button, user can see the alert dialog box
-    public void onBackPressed(){
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(SplashScreenActivity.this);
-        alertDialog.setTitle("Exit App");
-        alertDialog.setMessage("Do you want to exit this app ?");
-        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-              finishAffinity();
-            }
-        });
-        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
+
+    private void showNoInternetDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("No Internet Connection");
+        alertDialogBuilder.setMessage("Please check your network settings.")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Close the app or handle as needed
+                        finishAffinity();
+                    }
+                });
+
+        alertDialog = alertDialogBuilder.create();
         alertDialog.show();
+    }
+
+    // Implement device back button.
+    // If the user clicks the back button, the user can see the alert dialog box
+    public void onBackPressed() {
+        if (alertDialog != null && alertDialog.isShowing()) {
+            alertDialog.dismiss();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // Check internet connection method
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        }
+        return false;
+    }
+
+    // BroadcastReceiver to handle network state changes
+    private final BroadcastReceiver networkStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (isNetworkAvailable()) {
+                // Internet connection is restored, dismiss the dialog
+                if (alertDialog != null && alertDialog.isShowing()) {
+                    alertDialog.dismiss();
+                    setupSlideToUnlock();
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        // Unregister the BroadcastReceiver when the activity is destroyed
+        unregisterReceiver(networkStateReceiver);
+        super.onDestroy();
     }
 }
